354a8efe3962dfc9754c9c364e97830fbf62f7ee
Arif.A <arif.sain@gmail.com>
2025-10-09 14:18:23 +0200

    add Header component

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>
+          <a href="/">Heritage Explorer</a>
+        </h1>
+      </div>
+    </header>
+  );
+}
