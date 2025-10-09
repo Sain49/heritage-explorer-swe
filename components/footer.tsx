8c79cc2db60895c3ff322c8c02b66f5ae68ef8f5
Arif.A <arif.sain@gmail.com>
2025-10-09 15:42:55 +0200

    refactor Footer component to use a list

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer>
       <div>
-        <p>© 2025 Heritage Explorer. All rights reserved.</p>
+        <ul>
+          <li>© 2025 Heritage Explorer. All rights reserved.</li>
+          <li>
+            <Link href="https://docs.visitsweden.com/en/"></Link>Data sourced
+            from Visit Sweden National API
+          </li>
+          <li>
+            <Link href="/contact-us">Contact Us</Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
