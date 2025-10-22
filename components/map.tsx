d966f5c7c8479c44419ef6687e5860130017f042
Arif.A <arif.sain@gmail.com>
2025-10-22 11:47:22 +0200

    fix: update default marker icons for Leaflet in Next.js

--- components/map.tsx
+++ components/map.tsx
@@ -3,8 +3,20 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 import type { NominatimResult } from "@/types/site";
+
+// Fix for default marker icons in Next.js
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 export default function Map({ sites }: { sites: NominatimResult[] }) {
   const [isLoading, setIsLoading] = useState(true);
